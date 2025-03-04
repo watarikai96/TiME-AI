7743fc0709dc685e316ba57a424694d8d06e216c
watarikai96 <watarikai@outlook.com>
2025-03-04 18:47:14 +0900

    Initial commit with project structure

---
+++ backend/src/test/java/com/timeai/TimeAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timeai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
